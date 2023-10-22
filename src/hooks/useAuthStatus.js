7382f73c337670a85aaf1f2fc01eada5326d7afa
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-10-22 20:10:06 +0200

    adding editable user profile, and implementing private route, whihc means only authenticated user can access there

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,32 @@
+import { useEffect, useState, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+
+const useAuthStatus = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [checkingStatus, setCheckingStatus] = useState(true)
+    const isMounted = useRef(true)
+    useEffect(() => {
+        if (isMounted) {
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    setLoggedIn(true)
+                }
+                setCheckingStatus(false)
+            })
+        }
+
+        return () => {
+            isMounted.current = false
+        }
+
+    }, [isMounted])
+
+    return { loggedIn, checkingStatus }
+
+}
+
+export default useAuthStatus
+
+//onAuthStateChanged runs when the stance of the state changes, for Example when we go from logged in to logged out
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,101 @@
 import { useEffect, useState } from 'react'
-import { getAuth } from "firebase/auth"
+import { getAuth, updateProfile, updateEmail } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
+import { doc, updateDoc } from "firebase/firestore"
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 
 const Profile = () => {
-    const [user, setUser] = useState(null)
     const auth = getAuth()
-    useEffect(() => {
-        setUser(auth.currentUser)
-    }, [])
+    const [changeDetails, setChangeDetails] = useState(false)
 
-    return user ? <h1>{user.displayName}</h1> : <h1>Not Logged In</h1>
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email
+    })
+    const { name, email } = formData
+
+    const navigate = useNavigate()
+
+    const onLogOut = () => {
+        auth.signOut()
+        navigate("/")
+    }
+
+    const handleChange = (e) => {
+        setFormData((preState) => ({
+            ...preState,
+            [e.target.id]: e.target.value
+        }))
+    }
+    const onSubmit = async () => {
+
+        try {
+            //check to see if the current user is equal to actual name
+            if (auth.currentUser.displayName !== name) {
+                //Update display name in firebase
+                await updateProfile(auth.currentUser, {
+                    displayName: name
+                })
+
+
+
+                //Update in firestore
+                //we need to create a refrence
+                //the id in the firestore is equal with id in the authnetication
+                const userRef = doc(db, 'users', auth.currentUser.uid)
+                await updateDoc(userRef, {
+                    name,
+                    email
+                })
+            }
+
+        } catch (error) {
+            toast.error("Could not update profile Details.")
+        }
+    }
+
+
+    return <>
+        <div className="profile">
+            <header className="profileHeader">
+                <p className="pageHeader">My Profile</p>
+                <button onClick={onLogOut} className='logOut' type='button'>Logout</button>
+            </header>
+
+            <main>
+                <div className="profileDetailsHeader">
+                    <p className="profileDetailsText">Personal details</p>
+                    <p className="changePersonalDetails" onClick={() => {
+                        changeDetails && onSubmit()
+                        setChangeDetails((pre) => !pre)
+                    }}>
+                        {changeDetails ? 'Done' : "Change"}
+                    </p>
+                </div>
+
+                <div className="profileCard">
+                    <form>
+                        <input
+                            type="text"
+                            id='name'
+                            className={!changeDetails ? 'profileName' : 'profileNameActive'}
+                            disabled={!changeDetails}
+                            value={name}
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="email"
+                            id='email'
+                            className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+                            disabled={!changeDetails}
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </form>
+                </div>
+            </main >
+        </div >
+    </>
 }
 export default Profile
